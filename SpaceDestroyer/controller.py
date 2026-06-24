ff812a46c94b7c398ff2009ab95033520eaf88d2
LemonEatar <jakstr@outlook.de>
2026-06-24 08:35:55 +0200

    Rename Controller to CONTROLLER and implement init
    Refactor Controller class to CONTROLLER and add initialization.

--- SpaceDestroyer/controller.py
+++ SpaceDestroyer/controller.py
@@ -1,4 +1,13 @@
 from graphics_and_games_klassen import *
+from model import *
+from view import *
 
-class Controller:
-    def move:
+class CONTROLLER(EREIGNISBEHANDLUNG):
+    def __init__(self): 
+        super().__init__()
+        self.v = VIEW()
+        self.m = MODEL()
+    def TasteGedrueckt(self, taste):
+        if taste == "D":
+            self.v.ship.WinkelSetzen(20)
+c = CONTROLLER()
