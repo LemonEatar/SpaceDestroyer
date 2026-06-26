e756ec83825baba4500411fe52e09b037bbcafd9
LemonEatar <jakstr@outlook.de>
2026-06-26 09:20:09 +0200

    Implement bullet and enemy classes in view.py
    Added bullet and enemy classes with movement functionality.

--- SpaceDestroyer/view.py
+++ SpaceDestroyer/view.py
@@ -1,14 +1,39 @@
 from graphics_and_games_klassen import *
+import random 
 
 class VIEW(EREIGNISBEHANDLUNG):
 
     def __init__(self):
         super().__init__()
+        #Raumschiff
         self.ship = DREIECK()
         self.ship.PositionSetzen(500, 500)
         self.ship.GroesseSetzen(50, 100)
-        self.bullet = DREIECK()
-        self.bullet.SichtbarkeitSetzen(False)
-        self.bullet.GroesseSetzen(5,15)
-        self.bullet.FarbeSetzen("schwarz")
+        self.hitBoxPlayer = FIGUR(self.ship.x, self.ship.y + 25, 90)
+    
+    def movePlayer(self, taste):
+       
+    class BULLET(EREIGNISBEHANDLUNG):
 
+        def __init__(self, x, y):
+            super().__init__()
+            self.bullet = DREIECK()
+            self.bullet.PositionSetzen(x, y)
+            self.bullet.GroesseSetzen(5,15)
+            self.bullet.FarbeSetzen("schwarz")
+
+        def AktionAusfuehren(self):
+            print("executed")
+            self.bullet.PositionSetzen(self.bullet.x, self.bullet.y-1)
+        def TaktdauerSetzen(self, ms):
+            return super().TaktdauerSetzen(ms)
+        
+    class ENEMY(FIGUR):
+        
+        def __init__(self):
+            super().__init__()
+            self.FigurteilFestlegenEllipse(500, 50, 80, 80, "grün")
+
+        def AktionAusfuehren(self):
+            self.Drehen(random.randint(0,360))
+            self.Gehen(random.randint(1,30))
