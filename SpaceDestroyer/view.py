4b10859a4ac23190eda2208059b7c170c2c56440
Altschauerberg8 <fabi.strauch.2008@gmail.com>
2026-07-03 09:08:47 +0200

    Update view.py

--- SpaceDestroyer/view.py
+++ SpaceDestroyer/view.py
@@ -13,32 +13,7 @@ class VIEW(EREIGNISBEHANDLUNG):
     
     #def movePlayer(self, taste):
        
-    class BULLET(EREIGNISBEHANDLUNG):
+    class GAMELOOP(EREIGNISBEHANDLUNG):
 
-        def __init__(self, x, y):
-            super().__init__()
-            self.bullet = DREIECK()
-            self.bullet.PositionSetzen(x, y)
-            self.bullet.GroesseSetzen(5,15)
-            self.bullet.FarbeSetzen("schwarz")
-
-        def AktionAusfuehren(self):
-            print("executed")
-            self.bullet.PositionSetzen(self.bullet.x, self.bullet.y-1)
-        def TaktdauerSetzen(self, ms):
-            return super().TaktdauerSetzen(ms)
-        
-    class ENEMY(EREIGNISBEHANDLUNG):
-        
         def __init__(self):
             super().__init__()
-            self.enemy = FIGUR()
-            self.enemy.FigurteilFestlegenEllipse(500, 50, 80, 80, "grün")
-
-        def AktionAusfuehren(self):
-            #self.enemy.Drehen(random.randint(0,360))
-            self.enemy.PositionSetzen(self.enemy.x + 1, self.enemy.y + random.randint(-1, 1))
-            if self.enemy.x >= 800:
-                self.enemy.PositionSetzen(-200, 50)
-        def TaktdauerSetzen(self, ms):
-            return super().TaktdauerSetzen(ms)
