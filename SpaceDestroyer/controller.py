5ff834b9b44f202973a15f5f8ad7aa12325e18b5
Altschauerberg8 <fabi.strauch.2008@gmail.com>
2026-07-03 07:54:48 +0200

    Update controller.py

--- SpaceDestroyer/controller.py
+++ SpaceDestroyer/controller.py
@@ -9,19 +9,20 @@ class CONTROLLER(EREIGNISBEHANDLUNG):
         self.v = VIEW()
         self.m = MODEL()
     def TasteGedrueckt(self, taste):
-          if taste == 'a':
-            self.v.ship.Verschieben(-10, 0)
-            self.v.hitBoxPlayer.PositionSetzen(self.v.ship.x + 10, self.v.ship.y + 25)
-          if taste == 'd':
-            self.v.ship.Verschieben(10, 0)
-            self.v.hitBoxPlayer.PositionSetzen(self.v.ship.x, self.v.ship.y + 25)
-          if keyboard.is_pressed(' '):
-            self.v.ship.GanzNachHintenBringen()
-            bullet = self.v.BULLET(self.v.ship.x, self.v.ship.y)
-            bullet.TaktdauerSetzen(4)
-            print('Space pressed')
+        if taste == 'a':
+          self.v.ship.Verschieben(-10, 0)
+          self.v.hitBoxPlayer.PositionSetzen(self.v.ship.x + 10, self.v.ship.y + 25)
+        if taste == 'd':
+          self.v.ship.Verschieben(10, 0)
+          self.v.hitBoxPlayer.PositionSetzen(self.v.ship.x, self.v.ship.y + 25)
+        if keyboard.is_pressed(' '):
+          self.v.ship.GanzNachHintenBringen()
+          bullet = self.v.BULLET(self.v.ship.x, self.v.ship.y)
+          bullet.TaktdauerSetzen(4)
+          print('Space pressed')
     def Enemy(self):
-        self.enemy = self.v.ENEMY()
+        enemy = self.v.ENEMY()
+        enemy.TaktdauerSetzen(4)
 
 c = CONTROLLER()
 c.Enemy()
