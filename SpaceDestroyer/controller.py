045b455ca5167fd28f6dc24da2c6963d80bafb46
LemonEatar <jakstr@outlook.de>
2026-07-08 08:31:22 +0200

    Add enemy shooting and gametick management

--- SpaceDestroyer/controller.py
+++ SpaceDestroyer/controller.py
@@ -6,22 +6,34 @@ import keyboard
 class CONTROLLER(EREIGNISBEHANDLUNG):
     def __init__(self): 
         super().__init__()
+        self.gametick = 0
         self.v = VIEW()
         self.m = MODEL()
         self.enemy1 = self.m.ENEMY()
         self.bullets = []
+        self.enemyBullets = []
         super().TaktdauerSetzen(4)
 
-    #def TaktdauerSetzen(self, ms):
-       #return super().TaktdauerSetzen(ms)
-
     def AktionAusfuehren(self):
-       self.enemy1.MoveEnemy()
-       self.v.ship.GanzNachHintenBringen()
-       for bullet in self.bullets:
+      self.EnemyShooting()
+      self.gametick += 1
+      self.enemy1.MoveEnemy()
+      self.v.ship.GanzNachHintenBringen()
+      for bullet in self.bullets:
+        if bullet.bullet.y <= -10:
+          bullet.bullet.SichtbarkeitSetzen(False)
+          self.bullets.remove(bullet)
         bullet.MoveBullet()
         print('Space pressed')
-
+      for bullet1 in self.enemyBullets:
+        bullet1.MoveEnemyBullet()
+      
+      
+    def EnemyShooting(self):
+       if len(self.enemyBullets) == 0:
+          self.enemyBullets.append(self.m.BULLET(self.enemy1.enemy.x + 230, self.enemy1.enemy.y, "lila"))
+       if self.enemyBullets[-1].bullet.y >= 300:
+            self.enemyBullets.append(self.m.BULLET(self.enemy1.enemy.x + 230, self.enemy1.enemy.y, "lila")) 
 
     
     def TasteGedrueckt(self, taste):
@@ -32,7 +44,8 @@ class CONTROLLER(EREIGNISBEHANDLUNG):
           self.v.ship.Verschieben(10, 0)
           self.v.hitBoxPlayer.PositionSetzen(self.v.ship.x, self.v.ship.y + 25)
         if keyboard.is_pressed(' '):
-          self.bullets.append(self.m.BULLET(self.v.ship.x, self.v.ship.y))
+          self.bullets.append(self.m.BULLET(self.v.ship.x, self.v.ship.y, "schwarz"))
 
 
 c = CONTROLLER()
+
