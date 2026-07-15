ab88b574c224c498ea7d662cea8ad26e8292009d
LemonEatar <jakstr@outlook.de>
2026-07-15 09:21:13 +0200

    Implement game over state and lives tracking
    Added game over background and lives tracking to the VIEW class. Implemented game over state display when lives reach zero.

--- SpaceDestroyer/view.py
+++ SpaceDestroyer/view.py
@@ -5,16 +5,36 @@ class VIEW(EREIGNISBEHANDLUNG):
 
     def __init__(self):
         super().__init__()
+        self.gameoverbg = RECHTECK()
+        self.gameoverbg.PositionSetzen(-100, -100)
+        self.gameoverbg.FarbeSetzen("schwarz")
+        self.gameoverbg.GroesseSetzen(2000, 2000)
+        self.gameoverbg.SichtbarkeitSetzen(False)
+        self.gameoverstate = TEXT()
+        self.gameoverstate.TextGroesseSetzen(100)
+        self.gameoverstate.SichtbarkeitSetzen(False)
         #Raumschiff
         self.ship = DREIECK()
+        self.lives = 100
         self.ship.PositionSetzen(500, 500)
         self.ship.GroesseSetzen(50, 100)
-        self.hitBoxPlayer = FIGUR(self.ship.x, self.ship.y + 25, 90)
+        self.gameoverstate.TextSetzen(self.lives)
     def movePlayer(self, left):
         if left == True:
             self.ship.Verschieben(-1, 0)
         else:
             self.ship.Verschieben(1,0)
+    def __del__(self):
+        print("Player destroyed")
     def playerIsHit(self, ebulletx, ebullety):
         if self.ship.x + 10 > ebulletx > self.ship.x - 10 and ebullety >= self.ship.y:
-            print("player is hit")
+           self.lives -= 1
+           self.gameoverstate.TextSetzen(self.lives)
+           if self.lives <= 0:
+               self.gameoverbg.SichtbarkeitSetzen(True)
+               self.gameoverstate.SichtbarkeitSetzen(True)
+               self.gameoverstate.TextGroesseSetzen(250)
+               self.gameoverstate.TextSetzen("Game Over")
+               self.__del__
+           print(self.lives)
+
